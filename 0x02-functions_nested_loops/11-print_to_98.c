8d0e2b9c1175b3cee564e6fccf8a66b9f1537b5f
Nnenna12 <nnennaokereke@outlook.com>
2023-03-22 02:37:04 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,42 +4,27 @@
 
 /**
  * print_to_98 - print all natural numbers from n to 98
- * @n: The number to start printing from
- * Return: Always 0
+ * @n: starting integer
+ * Return: void
  */
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-	for (; n <= 98; n++)
+	while (n < 98)
 	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
-	}
-	else
-	{
-	printf("%d. ", n);
+	printf("%d, ", n);
+	n++;
 	}
 	}
-	}
-	else
+	else if (n > 98)
 	{
-	for (; n >= 98; n--)
+	while (n > 98)
 	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
-	}
-	else
-	{
-	printf("%d. ", n);
+	printf("%d, ", n);
+	n--;
 	}
 	}
-	}
+	printf("98\n");
 }
