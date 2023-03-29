a1627b4ce7a0407bfe3e062847e07aeafe8a9ef9
Nnenna12 <nnennaokereke@outlook.com>
2023-03-29 13:40:11 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,39 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 - enclose a string using rot13
- * @str: the string to be encoded
+ * rot13 - encoder rot13
+ * @s: pointer to string params
  *
- * Retrun: A pointer to the encoded string
+ * Retrun: *s
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
+	int i;
+	int j;
+	char data1[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
 			'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
 			'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
 			'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
 			'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 			'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
 			'v', 'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'Q', 'R', 'S', 'T', 'U',
+	char datarot[] = {'N', 'O', 'Q', 'R', 'S', 'T', 'U',
 			'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C',
 			'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 			'L', 'M', 'n', 'o', 'p', 'q', 'r', 's',
 			't', 'u', 'v', 'w', 'x', 'y', 'z', 'a',
 			'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
 			'j', 'k', 'l', 'm'};
-	while (str[indx1])
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	for (indx2 = 0; indx2 < 52; indx2++)
+	for (j = 0; j < 52; j++)
 	{
-	if (str[indx1] == alphabet[indx2])
+	if (s[i] == data1[j])
 	{
-	str[indx1] = rot13key[indx2];
+	s[i] = datarot[j];
+	break;
 	}
 	}
-	indx1++;
 	}
-	return (str);
+	return (s);
 }
